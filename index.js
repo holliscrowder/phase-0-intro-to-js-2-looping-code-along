7735d5ac8a43e80d8767d7727497a713014526d1
Hollis Crowder <hollis.crowder@gmail.com>
2024-02-26 13:01:37 -0800

    complete looping code along

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// create messages function
+let messages = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        let newMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages = [...messages, newMessage];
+    }
+    return messages;
+}
+
+function countDown(integer) {
+    for (let i = integer; i >= 0; i--) {
+        console.log(i);
+    }
+}
